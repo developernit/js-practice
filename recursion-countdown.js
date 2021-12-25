1aa3d01b6f292ab0e59c063e1470271f75774d10
Nitish Kafle <kaflenit@gmail.com>
2021-12-24 21:08:21 -0500

    solved using recursion

--- recursion-countdown.js
+++ recursion-countdown.js
@@ -12,3 +12,20 @@ function countdown(n){
       return newArr
     } 
   }
+
+/*
+We have defined a function named rangeOfNumbers with two parameters. The function should return an array of integers which begins with a number represented by the startNum parameter and ends with a number represented by the endNum parameter. 
+The starting number will always be less than or equal to the ending number. Your function must use recursion by calling itself and not use loops of any kind. It should also work for cases where both startNum and endNum are the same.
+*/
+  function rangeOfNumbers(startNum, endNum) {
+    if (startNum - endNum === 0){
+      return [startNum];
+    } else {
+      const newArr = rangeOfNumbers(startNum, endNum - 1);
+      newArr.push(endNum);
+      return newArr
+    }
+  };
+  
+  
+  console.log(rangeOfNumbers(6, 9))
